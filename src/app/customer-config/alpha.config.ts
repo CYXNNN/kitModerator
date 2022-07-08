f16162c1d64552715468dec2477aecd2c9999546
Christian Egli <info@christian-egli.ch>
2022-07-08 11:35:32 +0200

    #2: Alpha added

--- src/app/customer-config/alpha.config.ts
+++ src/app/customer-config/alpha.config.ts
@@ -1,7 +1,15 @@
 import {Config} from './_config';
+import {ITranslations} from './interface/ITranslations';
+import {AlphaTranslationsDe} from './translations/alpha/alpha.translations_de';
+import {AlphaTranslationsEn} from './translations/alpha/alpha.translations_en';
 
 export class AlphaConfig extends Config {
 
   readonly identifier = 'alpha';
+
+  translations: ITranslations = {
+    de: new AlphaTranslationsDe(),
+    en: new AlphaTranslationsEn(),
+  };
 
 }
