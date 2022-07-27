b278cd95503b6e519f970452c4943fe23fc4839f
Christian Egli <info@christian-egli.ch>
2022-07-27 15:24:44 +0200

    A_hierarchisch_4

--- src/app/customer-config/delta.config.ts
+++ src/app/customer-config/delta.config.ts
@@ -1,3 +1,4 @@
+import {FormField} from '../model/formField';
 import {Config} from './_config';
 import {DeltaTranslations} from './translations/delta/delta.translations';
 
@@ -5,5 +6,16 @@ export class DeltaConfig extends Config {
 
   readonly identifier = 'delta';
   translations = new DeltaTranslations();
+
+  forms = {
+    ...this.forms,
+    abwesenheitAdd: [
+      new FormField('toDate', 'datetime-local'),
+      new FormField('children', 'children-select'),
+      new FormField('fromDate', 'datetime-local'),
+      new FormField('reason'),
+      new FormField('comment', 'textarea', []),
+    ],
+  };
 
 }
