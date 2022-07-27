f9e95fdf62f2d843abb652ac2e0ecb62d5e39b64
Christian Egli <info@christian-egli.ch>
2022-07-27 15:36:05 +0200

    A_hierarchisch_6

--- src/app/customer-config/zeta.config.ts
+++ src/app/customer-config/zeta.config.ts
@@ -1,3 +1,4 @@
+import {FormField} from '../model/formField';
 import {Config} from './_config';
 import {ZetaTranslations} from './translations/zeta/zeta.translations';
 
@@ -6,4 +7,14 @@ export class ZetaConfig extends Config {
   readonly identifier = 'zeta';
   translations = new ZetaTranslations();
 
+  forms = {
+    ...this.forms,
+    abwesenheitAdd: [
+      new FormField('children', 'children-select'),
+      new FormField('fromDate', 'datetime-local'),
+      new FormField('toDate', 'datetime-local'),
+      new FormField('comment', 'textarea', []),
+      new FormField('reason'),
+    ],
+  };
 }
