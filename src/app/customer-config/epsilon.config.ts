48365563662d7fb9d3453f649618c7fc00a019d7
Christian Egli <info@christian-egli.ch>
2022-07-27 15:30:07 +0200

    A_hierarchisch_5

--- src/app/customer-config/epsilon.config.ts
+++ src/app/customer-config/epsilon.config.ts
@@ -1,3 +1,4 @@
+import {FormField} from '../model/formField';
 import {Config} from './_config';
 import {EpsilonTranslations} from './translations/epsilon/epsilon.translations';
 
@@ -6,4 +7,14 @@ export class EpsilonConfig extends Config {
   readonly identifier = 'epsilon';
   translations = new EpsilonTranslations();
 
+  forms = {
+    ...this.forms,
+    abwesenheitAdd: [
+      new FormField('toDate', 'datetime-local'),
+      new FormField('fromDate', 'datetime-local'),
+      new FormField('children', 'children-select'),
+      new FormField('reason'),
+      new FormField('comment', 'textarea', []),
+    ],
+  };
 }
