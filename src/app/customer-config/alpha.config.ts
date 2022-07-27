353e118cbbb5c3d0f8c57ad55f40184619b0fa92
Christian Egli <info@christian-egli.ch>
2022-07-27 15:14:51 +0200

    A_hierarchisch_1

--- src/app/customer-config/alpha.config.ts
+++ src/app/customer-config/alpha.config.ts
@@ -1,4 +1,5 @@
 import {Child} from '../model/child';
+import {FormField} from '../model/formField';
 import {Config} from './_config';
 import {AlphaTranslations} from './translations/alpha/alpha.translations';
 
@@ -12,5 +13,16 @@ export class AlphaConfig extends Config {
   };
 
   translations = new AlphaTranslations();
+
+  forms = {
+    ...this.forms,
+    abwesenheitAdd: [
+      new FormField('fromDate', 'datetime-local'),
+      new FormField('children', 'children-select'),
+      new FormField('toDate', 'datetime-local'),
+      new FormField('reason'),
+      new FormField('comment', 'textarea', []),
+    ],
+  };
 
 }
