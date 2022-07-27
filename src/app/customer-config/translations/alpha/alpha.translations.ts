8e91ae6a3dc36c8a32b92358c1d6a563efda7af1
Christian Egli <info@christian-egli.ch>
2022-07-27 17:15:14 +0200

    A_textuell_1

--- src/app/customer-config/translations/alpha/alpha.translations.ts
+++ src/app/customer-config/translations/alpha/alpha.translations.ts
@@ -1,5 +1,57 @@
 import {Translations} from '../_translations';
 
 export class AlphaTranslations extends Translations {
+  fromDate = 'From';
+  toDate = 'Until';
+  reason = 'Reason';
+  comment = 'Comment';
 
+  crud = {
+    created: 'message was submitted to the daycare center',
+    updated: 'changes have been saved',
+    deleted: 'entry has been deleted',
+  };
+
+  form = {
+    invalid: {
+      title: 'Form incomplete',
+      body: 'Please check your entries',
+    },
+    submit: 'Submit',
+    delete: 'Delete',
+  };
+
+  confirm = {
+    title: 'Are you sure?',
+    body: 'This action cannot be undone',
+    yes: 'Confirm',
+    no: 'Cancel',
+  };
+
+  button = {
+    add: 'Add',
+    all: 'All',
+    view: 'More',
+    delete: 'Delete',
+  };
+
+  children = {
+    title: 'Children',
+
+    select: {
+      placeholder: 'Select child(ren)',
+    },
+  };
+
+  dashboard = {
+    title: 'Dashboard',
+    welcome: 'Hello good day',
+  };
+
+  abwesenheiten = {
+    title: 'Absences',
+    form: {
+      notice: 'Fill out the form completely',
+    },
+  };
 }
