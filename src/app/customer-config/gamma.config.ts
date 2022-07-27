d9de93ef6c8e2399a5b33acdaf347c392cda0501
Christian Egli <info@christian-egli.ch>
2022-07-27 15:22:00 +0200

    A_hierarchisch_3

--- src/app/customer-config/gamma.config.ts
+++ src/app/customer-config/gamma.config.ts
@@ -1,3 +1,4 @@
+import {FormField} from '../model/formField';
 import {Config} from './_config';
 import {GammaTranslations} from './translations/gamma/gamma.translations';
 
@@ -6,4 +7,14 @@ export class GammaConfig extends Config {
   readonly identifier = 'gamma';
   translations = new GammaTranslations();
 
+  forms = {
+    ...this.forms,
+    abwesenheitAdd: [
+      new FormField('children', 'children-select'),
+      new FormField('toDate', 'datetime-local'),
+      new FormField('fromDate', 'datetime-local'),
+      new FormField('reason'),
+      new FormField('comment', 'textarea', []),
+    ],
+  };
 }
